40ccac53e906e1232fa46f1ddcba72bcbd56ee58
Alexandre Yeargar <eayeargan@gmail.com>
2025-02-23 14:42:08 -0500

    adds exported word bank for random use

---
+++ wordBank.js
@@ -0,0 +1,5 @@
+export const names = ['James', 'Jack', 'Jimmy', 'Jason', 'Jimbo'];
+
+export const actions = ['grabs', 'pokes', 'caresses', 'drops', 'fixes'];
+
+export const subjects = ['the robot', 'the donkey', 'the bicycle', 'the paper plate', 'the pants'];
